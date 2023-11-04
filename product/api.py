9fea23adf1e3b992baf97139b50a5e152924a1c9
Md. Rumon Chowdhury <52657264+rumonman@users.noreply.github.com>
2023-11-04 14:21:41 +0600

    Update api.py

--- product/api.py
+++ product/api.py
@@ -11,7 +11,7 @@ api = Api(app)
 class Product(Resource):
     def get(self):
         return {
-            'products': ['Ice cream', 'Chocolate', 'Apple', 'Mango']        }
+            'products': ['Ice cream', 'Chocolate', 'Apple', 'Mango', 'Orange', "Painapel"]        }
 
 # Create routes
 api.add_resource(Product, '/')
